d8644b8346ff6ed0e3b5d0e4e28dddd31cda0063
timothy ademola <timife007@gmail.com>
2024-10-18 11:28:06 +0100

    Upload progress

--- app/src/main/java/com/timife/cloudiapp/CloudiViewModel.kt
+++ app/src/main/java/com/timife/cloudiapp/CloudiViewModel.kt
@@ -1,16 +1,8 @@
 package com.timife.cloudiapp
 
-import android.content.Context
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import java.io.File
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
 import javax.inject.Inject
 
 
--- app/src/main/java/com/timife/cloudiapp/MainActivity.kt
+++ app/src/main/java/com/timife/cloudiapp/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.LinearProgressIndicator
@@ -24,17 +26,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
 import com.cloudinary.android.MediaManager
 import com.cloudinary.android.callback.ErrorInfo
 import com.cloudinary.android.callback.UploadCallback
 import com.timife.cloudiapp.ui.theme.CloudiAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -52,41 +56,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CloudiAppTheme {
-                var videoProgress by remember { mutableDoubleStateOf(0.0) }  // Track progress in state
-                var imageProgress by remember { mutableDoubleStateOf(0.0) }
+                val context = LocalContext.current
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
                     Column(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Screen(
-                            padding = innerPadding, onClick = {
-                                lifecycleScope.launch {
-                                    uploadVideo(
-                                        this@MainActivity,
-                                        onProgressTracked = { currentProgress ->
-                                            videoProgress = currentProgress
-                                        }
-                                    )
-                                }
-                            },
-                            progress = videoProgress,
+                        //Video upload
+                        UploadScreen(
+                            context = context,
+                            url = "https://res.cloudinary.com/dlujwccdb/video/upload/v1728910228/Stridez/posts/test_uy4d9t.mp4",
                             isVideo = true
                         )
-                        Screen(
-                            padding = innerPadding, onClick = {
-                                lifecycleScope.launch {
-                                    uploadImage(
-                                        this@MainActivity,
-                                        onProgressTracked = { currentProgress ->
-                                            imageProgress = currentProgress
-                                        }
-                                    )
-                                }
-                            },
-                            progress = imageProgress,
+                        Spacer(modifier = Modifier.height(16.dp))
+                        //Image upload
+                        UploadScreen(
+                            context = context,
+                            url = "https://res.cloudinary.com/dlujwccdb/video/upload/v1728910228/Stridez/posts/test_uy4d9t.jpg",
                             isVideo = false
                         )
                     }
@@ -96,118 +86,146 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    //Video upload
-    private suspend fun uploadVideo(context: Context, onProgressTracked: (Double) -> Unit) {
-        val url =
-            "https://res.cloudinary.com/dlujwccdb/video/upload/v1728910228/Stridez/posts/test_uy4d9t.mp4"
+    @Composable
+    fun UploadScreen(
+        context: Context,
+        url: String,
+        isVideo: Boolean,
+        scope: CoroutineScope = rememberCoroutineScope()
+    ) {
+        // Track the progress individually for each screen
+        var progress by remember { mutableDoubleStateOf(0.0) }
+        var uploaded by remember { mutableStateOf("") }
+
+        Screen(
+            padding = PaddingValues(),  // Adjust this as needed
+            onClick = {
+                scope.launch {
+                    uploadMedia(
+                        context = context,
+                        url = url,
+                        onProgressTracked = { bytes, totalBytes ->
+                            val currentProgress = bytes.toDouble() / totalBytes
+                            progress = currentProgress  // Update progress
+                            // Convert bytes to megabytes (MB) and update the uploaded state
+                            val uploadedMB = bytes / (1024.0 * 1024.0) // Bytes to MB
+                            val totalMB = totalBytes / (1024.0 * 1024.0) // Total MB
+
+                            uploaded = String.format(
+                                "%.2f MB / %.2f MB (%d%%)",
+                                uploadedMB,
+                                totalMB,
+                                (currentProgress * 100).toInt()
+                            )
+                        }
+                    )
+                }
+            },
+            progress = progress,
+            isVideo = isVideo,
+            onResetProgress = {
+                progress = 0.0  // Reset progress
+                uploaded = ""
+            },
+            uploaded = uploaded
+        )
+    }
+
+    @Composable
+    fun Screen(
+        padding: PaddingValues,
+        progress: Double,  // Receive progress value
+        onClick: () -> Unit,
+        isVideo: Boolean,
+        onResetProgress: () -> Unit = {},
+        uploaded: String
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            // Show progress as a ProgressBar
+            LinearProgressIndicator(
+                progress = { progress.toFloat() },
+                modifier = Modifier.fillMaxWidth(0.8f),
+            )
+
+            Text(text = "${(progress * 100).toInt()}%")
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = uploaded)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = onClick) {
+                    Text(text = if (isVideo) "Upload Video" else "Upload Image")
+                }
+
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = {
+                    onResetProgress()
+                }) {
+                    Text(text = "Reset Progress")
+                }
+            }
+
+        }
+    }
+
+    // Reusable upload function
+    private suspend fun uploadMedia(
+        context: Context,
+        url: String,
+        onProgressTracked: (Long, Long) -> Unit
+    ) {
         try {
             val file = withContext(Dispatchers.IO) {
                 downloadFile(url, context)
             }
+
             if (file != null) {
-                val requestId =
-                    MediaManager.get().upload(file.path).callback(object : UploadCallback {
-                        override fun onStart(requestId: String?) {
-                            Log.d("MainActivity", "onStart: $requestId")
-                        }
+                val requestId = MediaManager.get()
+                    .upload(file.path)
+                    .callback(createUploadCallback(onProgressTracked))
+                    .dispatch()
 
-                        override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
-                            Log.d("MainActivity", "onProgress: $bytes of $totalBytes")
-                            val progress = bytes.toDouble() / totalBytes
-                            onProgressTracked(progress)
-                            Log.d("MainActivity", "onProgress: $progress")
-
-                        }
-
-                        override fun onSuccess(
-                            requestId: String?,
-                            resultData: MutableMap<Any?, Any?>?
-                        ) {
-                            Log.d("MainActivity", "onSuccess: $resultData")
-                        }
-
-                        override fun onError(requestId: String?, error: ErrorInfo?) {
-                            Log.d("MainActivity", "onError: $error")
-                        }
-
-                        override fun onReschedule(requestId: String?, error: ErrorInfo?) {
-                            Log.d("MainActivity", "onReschedule: $error")
-                        }
-                    }).dispatch()
-                Log.d("MainActivity", "uploadVideo: $requestId")
+                Log.d("MainActivity", "uploadMedia: $requestId")
             } else {
-                Log.d("MainActivity", "uploadVideo: File is null")
+                Log.d("MainActivity", "uploadMedia: File is null")
             }
         } catch (e: Exception) {
-            Log.d("MainActivity", "uploadVideo: ${e.message}")
+            Log.e("MainActivity", "uploadMedia Error: ${e.message}")
         }
     }
 
-    //download image from url and upload
-    private suspend fun uploadImage(context: Context, onProgressTracked: (Double) -> Unit) {
-        val url =
-            "https://res.cloudinary.com/dlujwccdb/video/upload/v1728910228/Stridez/posts/test_uy4d9t.jpg"
-        try {
-            val file = withContext(Dispatchers.IO) {
-                downloadFile(url, context)
+    // Helper: Create a reusable UploadCallback
+    private fun createUploadCallback(onProgressTracked: (Long, Long) -> Unit) =
+        object : UploadCallback {
+            override fun onStart(requestId: String?) {
+                Log.d("MainActivity", "onStart: $requestId")
             }
 
-            if (file != null) {
-
-                val requestId =
-                    MediaManager.get().upload(file.path).callback(object : UploadCallback {
-                        override fun onStart(requestId: String?) {
-                            Log.d("MainActivity", "onStart: $requestId")
-                        }
-
-                        override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
-                            val progress = bytes.toDouble() / totalBytes
-                            onProgressTracked(progress)
-                            Log.d("MainActivity percentage", "onProgress: $progress")
-                            Log.d("MainActivity", "onProgress: $bytes of $totalBytes")
-                        }
-
-                        override fun onSuccess(
-                            requestId: String?,
-                            resultData: MutableMap<Any?, Any?>?
-                        ) {
-                            Log.d("MainActivity", "onSuccess: $resultData")
-                        }
-
-                        override fun onError(requestId: String?, error: ErrorInfo?) {
-                            Log.d("MainActivity", "onError: $error")
-                        }
-
-                        override fun onReschedule(requestId: String?, error: ErrorInfo?) {
-                            Log.d("MainActivity", "onReschedule: $error")
-                        }
-                    }).dispatch()
-                Log.d("MainActivity", "uploadImage: $requestId")
-            } else {
-                Log.d("MainActivity", "uploadImage: File is null")
+            override fun onProgress(requestId: String?, bytes: Long, totalBytes: Long) {
+                onProgressTracked(bytes, totalBytes)
+                Log.d("MainActivity", "Progress:($bytes of $totalBytes)")
             }
-        } catch (e: Exception) {
-            Log.d("Cloudinary", "uploadImage: ${e.message}")
+
+            override fun onSuccess(requestId: String?, resultData: MutableMap<Any?, Any?>?) {
+                Log.d("MainActivity", "onSuccess: $resultData")
+            }
+
+            override fun onError(requestId: String?, error: ErrorInfo?) {
+                Log.e("MainActivity", "onError: $error")
+            }
+
+            override fun onReschedule(requestId: String?, error: ErrorInfo?) {
+                Log.w("MainActivity", "onReschedule: $error")
+            }
         }
 
-    }
-
-//    //upload large video
-//    private suspend fun uploadLargeVideo(context: Context) {
-//        val url =
-//            "https://res.cloudinary.com/dlujwccdb/video/upload/v1728910228/Stridez/posts/test_uy4d9t.mp4"
-//
-//        try {
-//            val file = withContext(Dispatchers.IO) {
-//                downloadFile(url, context)
-//            }
-//            if (file != null) {
-//                val requestId = MediaManager.get().upload()
-//            }
-//        }catch (e:Exception){
-//            Log.d("MainActivity", "uploadLargeVideo: ${e.message}")
-//        }
-//    }
 
     // Helper function to download a file from a URL
     private fun downloadFile(fileUrl: String, context: Context): File? {
@@ -223,37 +241,6 @@ class MainActivity : ComponentActivity() {
         }
 
         return file
-    }
-
-}
-
-@Composable
-fun Screen(
-    padding: PaddingValues,
-    progress: Double,  // Receive progress value
-    onClick: () -> Unit,
-    isVideo: Boolean
-) {
-    Column(
-        modifier = Modifier
-            .wrapContentSize()
-            .padding(padding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        // Show progress as a ProgressBar
-        LinearProgressIndicator(
-            progress = { progress.toFloat() },
-            modifier = Modifier.fillMaxWidth(0.8f),
-        )
-
-        Text(text = "${(progress * 100).toInt()}%")
-        Button(onClick = onClick) {
-            Text(text = if (isVideo) "Upload Video" else "Upload Image")
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
     }
 }
 
